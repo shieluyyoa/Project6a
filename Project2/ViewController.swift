d6b4bf3d6bedaee286dc37b55ac755e88415012e
shieluyyoa <luiwinghin8998@gmail.com>
2022-04-14 00:57:36 +0800

    Project6

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -9,11 +9,71 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    var countries:[String] = []
+    var score = 0
+    var correctAnswer = 0
+    var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        
+        askQuestion()
+        
+        
         // Do any additional setup after loading the view.
     }
 
+    func askQuestion(action:UIAlertAction!=nil) {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        button1.setImage(UIImage(named:countries[0]), for: .normal)
+        button2.setImage(UIImage(named:countries[1]), for: .normal)
+        button3.setImage(UIImage(named:countries[2]), for: .normal)
+        
+        title = countries[correctAnswer].uppercased()+"  \(score)"
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title:String
+        var message:String
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+            message = "Your score is \(score)"
+        } else {
+            title = "Wrong"
+            score -= 1
+            message = "Wrong ! That's the flag of \(countries[sender.tag])"
+        }
+        counter += 1
+        if counter == 10 {
+            message += "\n\nYour final score is \(score)"
+            score = 0
+            
+        }
+        
+            let ac = UIAlertController(title: title, message: message , preferredStyle: .alert)
+        
+            ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+            present(ac,animated: true)
+        }
+        
+        
+        
+        
+    }
 
-}
 
